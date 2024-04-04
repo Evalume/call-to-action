1bd703b150fe24028f3ce8d43d29010718ce6709
Evalume <164504893+Evalume@users.noreply.github.com>
2024-04-04 20:16:54 +0200

    Checkboxes fixed and other small changes

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+function sendMail() {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail.emailAddr.value))
+    {
+      emailjs.init("fZtOnSTeaLmAHsULx")
+      emailjs.send("service_yivyy9j","template_7n7vkdj",{
+      subject: "This is test",
+      message: "This is test message",
+      });
+    }
+    else{
+      mail.emailAddr.style.color = "red";
+    }   
+  }
