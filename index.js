6a94403f901f0a5aad345955dafd0444583dd52e
Evalume <164504893+Evalume@users.noreply.github.com>
2024-04-05 09:11:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ function sendMail() {
     {
       emailjs.init("fZtOnSTeaLmAHsULx")
       emailjs.send("service_yivyy9j","template_7n7vkdj",{
-      subject: "This is test",
-      message: "This is test message",
+      send_to: "evalumeproject@gmail.com",
+      subject: mail.emailAddr.value.toString(),
+      message: "Email is subject, add it to a excel list or something",
+      });
+
+      emailjs.send("service_yivyy9j","template_7n7vkdj",{
+      send_to: mail.emailAddr.value.toString(),
+      subject: "Welcome to zombo.com",
+      message: "everything is possible at zombo dot com",
       });
     }
     else{
